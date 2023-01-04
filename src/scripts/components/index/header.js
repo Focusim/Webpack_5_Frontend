1ed5405cce0a60ca7623d977c1771e9509963c03
RomanG <r.garanin@intensa.ru>
2023-01-04 19:18:39 +0300

    day 04.01

--- src/scripts/components/index/header.js
+++
@@ -1,9 +0,0 @@
-(function (){
-	const header = document.querySelector('header.header');
-	const burger = header.querySelectorAll('[data-burger-btn]');
-	burger.forEach(element => {
-		element.addEventListener('click', (event) => {
-			document.body.classList.toggle('burger-menu--open')
-		});
-	});
-}());
--- src/scripts/pages/index.js
+++ src/scripts/pages/index.js
@@ -1,4 +1,3 @@
-import '../components/index/header';
 import '../components/index/scroll';
 import { createApp } from 'vue';
 import Vue3LottieApp from 'vue3-lottie'
@@ -9,7 +8,15 @@ import vueLaunchMoreLottie from "@/vue/pages/index/launch-more.vue";
 class MainPage {
 	constructor() {
 
+		this.state = {
+			header: {
+				selector: document.querySelector('header.header'),
+				burger: document.querySelectorAll('[data-burger-btn]'),
+			}
+		}
+
 		this.init();
+		this.burgerHandler(this.state.header.burger, document.body);
 		this.vueComponents([vueHeadLottie, '#vue-head']);
 		this.vueComponents([vueLaunchMoreLottie, '#vue-launch-more']);
 		this.vueComponents([vueForm, '#vue-form-start']);
@@ -17,6 +24,14 @@ class MainPage {
 
 	init() {
 		console.log('init JS main page');
+	}
+
+	burgerHandler(arrBurger, body) {
+		arrBurger.forEach(element => {
+			element.addEventListener('click', (event) => {
+				body.classList.toggle('burger-menu--open')
+			});
+		});
 	}
 
 	vueComponents([component, selector]) {
