1f4eae9ede8c686b655fc5dedaa53762c53b7c46
RomanG <r.garanin@intensa.ru>
2023-01-04 18:38:40 +0300

    day 04.01

--- postcss.config.js
+++ postcss.config.js
@@ -22,7 +22,20 @@ module.exports = {
 		} : {
 			'postcss-px-to-viewport': {
 				// options
+				unitToConvert: 'px',
 				viewportWidth: 1280,
+				unitPrecision: 5,
+				propList: ['*'],
+				viewportUnit: 'vw',
+				fontViewportUnit: 'vw',
+				selectorBlackList: [],
+				minPixelValue: 1,
+				mediaQuery: false,
+				replace: true,
+				exclude: [],
+				landscape: false,
+				landscapeUnit: 'vw',
+				landscapeWidth: 568
 			},
 			'postcss-preset-env': {
 				browsers: 'last 3 versions',
